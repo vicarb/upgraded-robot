93f015928113e58aa7aaaafca28574d3840be376
vicarb <vcardenasb@protonmail.com>
2023-04-30 12:06:34 -0400

    updated mobile layout

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
 import Link from 'next/link';
 import { Product } from '@/types/Product';
-const products = [
+
+export const products = [
     {
       id: 1,
       name: 'Men’s Sleeveless Shirt',
@@ -79,32 +80,34 @@ const ProductList = ({ products }: Props) => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-    <div className="flex justify-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-        {/* Replace `products` with your own array of products */}
-        {products.map((product) => (
-          <div key={product.id} className="bg-white p-4 rounded-md shadow-md transition duration-300 transform hover:scale-95 hover:shadow-lg relative" onMouseEnter={() => setHoveredProductId(product.id)} onMouseLeave={() => setHoveredProductId(null)}>
-            <Link href={`/products/${product.id}`}>
-              <span className="absolute top-0 left-0 w-full h-full"></span>
-            </Link>
-            <div className="h-48 w-full relative">
-              <img src={product.image} alt={product.name} className="h-full w-full object-cover rounded-md" />
-              <div className="absolute bottom-0 right-0 mr-4 mb-4 bg-gray-800 text-white p-1 rounded-md">
-                ${product.price.toFixed(2)}
-              </div>
+  <div className="flex justify-center">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4 md:gap-8">
+      {/* Replace `products` with your own array of products */}
+      {products.map((product) => (
+        <div key={product.id} className="bg-white p-4 rounded-md shadow-md transition duration-300 transform hover:scale-95 hover:shadow-lg relative" onMouseEnter={() => setHoveredProductId(product.id)} onMouseLeave={() => setHoveredProductId(null)}>
+          <Link href={`/products/${product.id}`}>
+            <span className="absolute top-0 left-0 w-full h-full"></span>
+          </Link>
+          <div className="h-48 w-full relative">
+            <img src={product.image} alt={product.name} className="md:h-full md:w-full object-cover rounded-md" />
+            <div className="absolute bottom-0 right-0 mr-4 mb-4 bg-gray-800 text-white p-1 rounded-md">
+              ${product.price.toFixed(2)}
             </div>
-            <h2 className="mt-2 text-gray-800 font-semibold text-lg">{product.name}</h2>
-            <p className="mt-1 text-gray-600">{product.description}</p>
-            {hoveredProductId === product.id && (
-              <button onClick={() => handleAddToCart(product)} className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-gray-800 via-gray-800 to-transparent text-white px-4 py-2 rounded-md flex justify-center items-center hover:from-gray-700 shadow-lg shadow-green-transparent transition duration-300">
-                <FaShoppingCart className="inline-block mr-2" /> Add to Cart
-              </button>
-            )}
           </div>
-        ))}
-      </div>
+          <h2 className="mt-2 text-gray-800 font-semibold text-lg">{product.name}</h2>
+          <p className="mt-1 text-gray-600 truncate">{product.description}</p>
+          {hoveredProductId === product.id && (
+            <button onClick={() => handleAddToCart(product)} className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-gray-800 via-gray-800 to-transparent text-white px-4 py-2 rounded-md flex justify-center items-center hover:from-gray-700 shadow-lg shadow-green-transparent transition duration-300">
+              <FaShoppingCart className="inline-block mr-2" /> Add to Cart
+            </button>
+          )}
+        </div>
+      ))}
     </div>
   </div>
+</div>
+
+
   );
 };
 
