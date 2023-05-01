81b4d70071dbb4ac9d9ee410b0ae6c3bdffeb8c9
vicarb <vcardenasb@protonmail.com>
2023-05-01 12:24:24 -0400

    added dynamic detail page

---
+++ pages/products/[id].tsx
@@ -0,0 +1,63 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
+import { Product } from '@/types/Product';
+import { log } from 'console';
+
+interface Props {
+  product: Product;
+}
+
+const ProductDetail: React.FC<Props> = ({ product }) => {
+  console.log("product from inside", product);
+  
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-pink-500 via-red-500 to-orange-500 text-white">
+      <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <div className="relative w-full h-80">
+            <Image src={product.image} alt={product.name} layout="fill" objectFit="cover" />
+          </div>
+          <div>
+            <h1 className="text-4xl font-bold mb-4">{product.name}</h1>
+            <p className="text-lg mb-6">{product.description}</p>
+            <div className="flex items-center">
+              <span className="text-3xl font-bold">${product.price.toFixed(2)}</span>
+              <button className="px-4 py-2 ml-6 bg-white text-gray-800 text-sm font-semibold rounded-md shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetail;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch('https://singular-marzipan-df9366.netlify.app/api/products');
+  const data = await response.json();
+
+  const paths = data.products.map((product: Product) => ({
+    params: { id: product._id.toString() },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    if (!params) {
+        return {
+          notFound: true,
+        };
+      }
+    
+    const { id } = params;
+    const response = await fetch(`https://singular-marzipan-df9366.netlify.app/api/products/${id}`);
+    const product = await response.json();
+    console.log("prod", product);
+    
+    return { props: { product } };
+  };
+  
--- types/Product.ts
+++ types/Product.ts
@@ -1,5 +1,6 @@
 export interface Product {
     id: number;
+    _id: number;
     name: string;
     description: string;
     price: number;
