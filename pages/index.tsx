f9f449f805e17c3e33fe5641a378e99ca553d1b2
vicarb <vcardenasb@protonmail.com>
2023-05-01 10:46:37 -0400

    Added StaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import ProductList from '@/components/ProductList/ProductList'
 import ImageSection from '@/components/ImageSection/ImageSection'
 import { Product } from '@/types/Product'
 import PList from '@/components/PList'
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 const inter = Inter({ subsets: ['latin'] })
 
 interface Props {
@@ -22,7 +23,7 @@ const images = [
 export default function Home({ products }: Props) {
   return (
    <>
-   <h1 className='text-4xl font-bold text-center mt-8'>Gym Clothing</h1>
+   <h1 className='text-4xl font-bold text-center mt-8'>AIRON Gym Clothing</h1>
    <ImageSection images={images} />
    <ProductList products={products} />
    <PList/>
@@ -31,69 +32,14 @@ export default function Home({ products }: Props) {
 }
 
 // Fetch data at build time
-export async function getStaticProps() {
-  const products = [
-    {
-      id: 1,
-      name: 'Men’s Sleeveless Shirt',
-      image: 'https://via.placeholder.com/150',
-      price: 24.99,
-      description: 'A comfortable and stylish sleeveless shirt for men.'
-    },
-    {
-      id: 2,
-      name: 'Women’s Leggings',
-      image: 'https://via.placeholder.com/150',
-      price: 34.99,
-      description: 'Comfortable leggings with a flattering fit for women.'
-    },
-    {
-      id: 3,
-      name: 'Men’s Shorts',
-      image: 'https://via.placeholder.com/150',
-      price: 29.99,
-      description: 'Lightweight and breathable shorts for men.'
-    },
-    {
-      id: 4,
-      name: 'Women’s Tank Top',
-      image: 'https://via.placeholder.com/150',
-      price: 19.99,
-      description: 'A classic tank top for women.'
-    },
-    {
-      id: 5,
-      name: 'Men’s T-Shirt',
-      image: 'https://via.placeholder.com/150',
-      price: 19.99,
-      description: 'A basic and comfortable t-shirt for men.'
-    },
-    {
-      id: 6,
-      name: 'Women’s Sports Bra',
-      image: 'https://via.placeholder.com/150',
-      price: 24.99,
-      description: 'A supportive and comfortable sports bra for women.'
-    },
-    {
-      id: 7,
-      name: 'Men’s Athletic Pants',
-      image: 'https://via.placeholder.com/150',
-      price: 39.99,
-      description: 'Sweat-wicking and comfortable pants for men.'
-    },
-    {
-      id: 8,
-      name: 'Women’s Athletic Shorts',
-      image: 'https://via.placeholder.com/150',
-      price: 29.99,
-      description: 'Comfortable and breathable shorts for women.'
-    }
-  ];
-
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const response = await fetch('https://singular-marzipan-df9366.netlify.app/api/products');
+  const data = await response.json();
+  
   return {
     props: {
-      products,
+      products: data.products,
     },
+    revalidate: 60, // revalidate every 60 seconds (1 minute)
   };
-}
+};
