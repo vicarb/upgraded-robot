6f305455a6764141d4c84d5ff9c42bc1a052b139
vicarb <vcardenasb@protonmail.com>
2023-05-02 09:21:50 -0400

    detail page update

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,15 +1,28 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { Product } from '@/types/Product';
-import { log } from 'console';
 
 interface Props {
   product: Product;
 }
 
 const ProductDetail: React.FC<Props> = ({ product }) => {
-  console.log("product from inside", product);
+  const router = useRouter();
+  const [cartItems, setCartItems] = useState<Product[]>([]);
+
+  const addToCart = () => {
+    setCartItems([...cartItems, product]);
+    console.log(cartItems);
+    
+  };
   
+
+  const cartTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price, 0);
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-pink-500 via-red-500 to-orange-500 text-white">
       <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-8">
@@ -22,7 +35,7 @@ const ProductDetail: React.FC<Props> = ({ product }) => {
             <p className="text-lg mb-6">{product.description}</p>
             <div className="flex items-center">
               <span className="text-3xl font-bold">${product.price.toFixed(2)}</span>
-              <button className="px-4 py-2 ml-6 bg-white text-gray-800 text-sm font-semibold rounded-md shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
+              <button className="px-4 py-2 ml-6 bg-white text-gray-800 text-sm font-semibold rounded-md shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out" onClick={addToCart}>
                 Add to Cart
               </button>
             </div>
@@ -34,6 +47,7 @@ const ProductDetail: React.FC<Props> = ({ product }) => {
 };
 
 export default ProductDetail;
+
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const response = await fetch('https://singular-marzipan-df9366.netlify.app/api/products');
