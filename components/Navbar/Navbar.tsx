97982a41bc4cac5c62e9ed9be5532fe6a1c89d7a
vicarb <vcardenasb@protonmail.com>
2023-05-02 15:06:28 -0400

    navbar layout

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,18 +1,18 @@
-
 import React, { useState } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
+import Link from 'next/link';
 
-interface NavbarProps {
-  
-}
+interface NavbarProps {}
 
-const Navbar: React.FC<NavbarProps> = ({  }) => {
+const Navbar: React.FC<NavbarProps> = ({}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-orange-500 to-yellow-500 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">Gym Clothing</span>
+        <Link href="/">
+          <span className="font-semibold text-xl tracking-tight">Gym Clothing</span>
+        </Link>
       </div>
       <div className="block lg:hidden">
         <button
@@ -25,29 +25,41 @@ const Navbar: React.FC<NavbarProps> = ({  }) => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <title>Menu</title>
-            <path
-              d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"
-            />
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
         </button>
       </div>
-      <div className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? 'block' : 'hidden'}`}>
+      <div
+        className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${
+          isOpen ? 'block' : 'hidden'
+        }`}
+      >
         <div className="text-sm lg:flex-grow lg:text-right">
-          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-800 mr-4"
+          >
             Docs
           </a>
-          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-800 mr-4"
+          >
             Examples
           </a>
-          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white">
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-800"
+          >
             Blog
           </a>
         </div>
         <div className="flex items-center lg:ml-4">
-          <div className="flex items-center ">
-            <FaShoppingCart size={20} className="text-white flex-1 cursor-pointer" />
-            
-          </div>
+          <Link href="/cart">
+            <span className="flex items-center text-white hover:text-gray-800">
+              <FaShoppingCart size={20} className="flex-1 cursor-pointer" />
+            </span>
+          </Link>
         </div>
       </div>
     </nav>
