37c450248a8196e3a76bea200aa4552da36ceb52
vicarb <vcardenasb@protonmail.com>
2023-05-03 00:17:01 -0400

    fixed add to cart bug

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -19,18 +19,22 @@ const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => 
   const [cartItems, setCartItems] = useState<Product[]>([]);
 
   const addToCart = (product: Product) => {
-    const itemIndex = cartItems.findIndex((item) => item.id === product.id);
-    if (itemIndex !== -1) {
-      const updatedCartItems = [...cartItems];
-      console.log("notupdated quantity", updatedCartItems[itemIndex].quantity);
-      updatedCartItems[itemIndex].quantity += 1;
-      console.log("updated quantity", updatedCartItems[itemIndex].quantity);
-      
-      setCartItems(updatedCartItems);
-    } else {
-      setCartItems([...cartItems, { ...product, quantity: 1 }]);
-    }
+    setCartItems(prevCartItems => {
+      const itemIndex = prevCartItems.findIndex((item) => item._id === product._id);
+      if (itemIndex !== -1) {
+        const updatedCartItems = [...prevCartItems];
+        updatedCartItems[itemIndex] = {
+          ...prevCartItems[itemIndex],
+          quantity: prevCartItems[itemIndex].quantity + 1,
+        };
+  
+        return updatedCartItems;
+      } else {
+        return [...prevCartItems, { ...product, quantity: 1 }];
+      }
+    });
   };
+  
   
 
   const clearCart = () => {
---
+++ pages/cart.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cart from '@/components/Cart/Cart'
+export default function cart() {
+  return (
+    <>
+    <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-8">
+    <h1 className="text-3xl font-bold">Cart</h1>
+    <Cart />
+  </div>
+  </>
+  )
+}
