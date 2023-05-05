75c4e3b98127fe440e3a530abc00f5e68f47745b
vicarb <vcardenasb@protonmail.com>
2023-05-05 02:28:02 -0400

    fixed margin

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -39,6 +39,7 @@ const Dashboard = () => {
     const height = chartSize.height - margin.top - margin.bottom;
 
     const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
+    
     const padding = 40
 
     const x = d3.scaleBand()
@@ -85,7 +86,7 @@ const Dashboard = () => {
 
   return (
     <div className="container mx-auto p-4">
-    <h1 className="text-2xl font-semibold mb-4">Dashboard</h1>
+    <h1 className="text-2xl font-semibold mb-4 text-white mt-16">Dashboard</h1>
     <div className="w-full h-full">
       <svg ref={chartRef} preserveAspectRatio="xMinYMin meet" viewBox={`0 0 ${chartSize.width} ${chartSize.height}`}></svg>
     </div>
